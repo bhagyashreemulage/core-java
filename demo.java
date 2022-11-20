1d0eae9cd52352ca8a79967f68e5977fcaba815a
bhagyashreemulage <118680334+bhagyashreemulage@users.noreply.github.com>
2022-11-20 22:06:46 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,12 @@
+import backend.*;
+import java.util.*;
+class demo 
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter limit= ");
+int limit = sc.nextInt();
+fibo.show(limit);
+}
+}
